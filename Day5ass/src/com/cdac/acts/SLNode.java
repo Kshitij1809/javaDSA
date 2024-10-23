91649af05f310df265fdfd8143bffd6683ab8f6d
abc <abc@acb>
2024-10-23 21:23:12 +0530

    Fifth day

---
+++ Day5ass/src/com/cdac/acts/SLNode.java
@@ -0,0 +1,16 @@
+package com.cdac.acts;
+
+public class SLNode {
+	int data;
+	SLNode next;
+	
+	public SLNode() {
+		data = 0;
+		next = null;
+	}
+	
+	public SLNode(int data) {
+		this.data = data;
+		next = null;
+	}
+}
